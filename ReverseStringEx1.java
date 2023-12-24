5c3667befd65d503ac7421309363d9bff1a362a1
praveen-golagani <praveen.gyj@gmail.com>
2023-12-24 23:51:27 +0530

    Reverse string without order change program

--- ReverseStringEx1.java
+++ ReverseStringEx1.java
@@ -8,17 +8,18 @@ public class ReverseStringEx1 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a sentence:");
         String input = sc.nextLine();
-        System.out.println("Reversed String without position change : "+rev(input));
+        System.out.println("Reversed String without position change : " + rev(input));
+        sc.close();
     }
 
-    public static String rev(String inp){
+    public static String rev(String inp) {
         String rev = "";
         String[] inp_str = inp.split(" ");
-        for(int i = 0; i<inp_str.length;i++){
-            for(int j = inp_str[i].length()-1;j>=0;j--){
+        for (int i = 0; i < inp_str.length; i++) {
+            for (int j = inp_str[i].length() - 1; j >= 0; j--) {
                 rev += inp_str[i].charAt(j);
             }
-            rev+= " ";
+            rev += " ";
         }
         return rev.trim();
     }
