e5f7fb13efffc38c8700cf3b387d21a1c07a654c
praveen-golagani <praveen.gyj@gmail.com>
2024-01-10 07:43:15 +0530

    updated folder structure

---
+++ src/original/ReverseString.java
@@ -0,0 +1,36 @@
+package original;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static String rev(String inp) {
+        String rev = "";
+        for (int i = inp.length() - 1; i >= 0; i--) {
+            rev += inp.charAt(i);
+        }
+        return rev;
+    }
+
+    public String revBuilder(String inp) {
+        StringBuilder sbld = new StringBuilder(inp);
+        return sbld.reverse().toString();
+    }
+
+    public String revBuff(String inp) {
+        StringBuffer sbuf = new StringBuffer(inp);
+        return sbuf.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ReverseString rs = new ReverseString();
+        System.out.println("Enter word/sentence : ");
+        String input = sc.nextLine();
+        System.out.println("Reverse : " + rev(input));
+        System.out.println("Reverse using Builder : " + rs.revBuilder(input));
+        System.out.println("Reverse using Buffer : " + rs.revBuff(input));
+        sc.close();
+    }
+}
